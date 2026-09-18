3138bfbf188c031b50d31672bbf16a5f561ad32b
Xing Long Ji <33090500+Xing97@users.noreply.github.com>
2026-09-18 16:54:07 +0200

    Change request matcher to permit all for API endpoints

--- src/main/java/es/ubu/lsi/ubumonitoranalytics/shared/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/es/ubu/lsi/ubumonitoranalytics/shared/infrastructure/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/**")
                     .authenticated() // Endpoints
                     .anyRequest()
-                    .denyAll())
+                    .permitAll())
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .oauth2ResourceServer(
             oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(sessionConverter)))
